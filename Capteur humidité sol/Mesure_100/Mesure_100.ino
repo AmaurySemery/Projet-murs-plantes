00f6110ee4a86885f094eb6fd4891c0db73df2f3
Amaury Semery <amaury.semery@laposte.net>
2020-09-02 17:43:18 +0200

    0

--- Capteur humidité sol/Mesure_100/Mesure_100.ino
+++ Capteur humidité sol/Mesure_100/Mesure_100.ino
@@ -10,8 +10,13 @@ pinMode(PinLed,OUTPUT);
 void loop() {
 // read the input on analog pin 0:
 int sensorValue = analogRead(A0); Serial.println(sensorValue);
-int sensorConvert = ((sensorValue*43,5)*20)/8900.1;
-if (sensorValue > 600) {digitalWrite(PinLed,HIGH);}
+byte a = 43.5;
+byte b = 20;
+byte c = 8900.1;
+int sensorConvert = sensorValue * a; 
+int sensorConvert1 = sensorConvert * b;
+int sensorConvert2 = sensorConvert1 / c; Serial.println(sensorConvert2);
+if (sensorConvert2 > 60) {digitalWrite(PinLed,HIGH);}
 else {digitalWrite(PinLed,LOW);}
 delay(10000);
 }
