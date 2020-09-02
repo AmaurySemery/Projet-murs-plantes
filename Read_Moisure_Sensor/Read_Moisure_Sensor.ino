8b0e68c5035bbba47536b21659a22c06421deb53
Amaury Semery <amaury.semery@laposte.net>
2020-09-02 14:34:05 +0200

    Create Read_Moisure_Sensor.ino

---
+++ Read_Moisure_Sensor/Read_Moisure_Sensor.ino
@@ -0,0 +1,18 @@
+int PinLed=3;
+
+void setup() {
+// initialize serial communication at 9600 bits per second:
+Serial.begin(9600);
+pinMode(PinLed,OUTPUT);
+}
+
+
+void loop() {
+// read the input on analog pin 0:
+int sensorValue = analogRead(A0); Serial.println(sensorValue);
+if (sensorValue < 300) {digitalWrite(PinLed,HIGH);}
+else {digitalWrite(PinLed,LOW);}
+delay(5000);
+}
+
+// Plus la terre sera humide, plus la valeur s’approchera de 0, plus elle sera sèche, plus la valeur s’approchera de 1023.
