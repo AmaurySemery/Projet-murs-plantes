1dc6ddb5264892f6a019db3e171997c5a8895ce6
Amaury Semery <amaury.semery@laposte.net>
2020-09-04 14:10:58 +0200

    Update Read_Moisure_Sensor_esp32_lora.ino

--- Communication Lora/Read_Moisure_Sensor_esp32_lora/Read_Moisure_Sensor_esp32_lora.ino
+++ Communication Lora/Read_Moisure_Sensor_esp32_lora/Read_Moisure_Sensor_esp32_lora.ino
@@ -1,9 +1,25 @@
-const int entreeAnalogique = 15;
-int PinLed=2;
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+#include "images.h"
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISnO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND  433E6
+
+unsigned int counter = 0;
 
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
+
+const int entreeAnalogique = 15;
+int PinLed=2;
 
 void setup() {
 // initialize serial communication at 9600 bits per second:
@@ -29,6 +45,18 @@ Serial.println("% d'humidité.");
 if (sensorConvert1 < 35) {digitalWrite(PinLed,HIGH);}
 else {digitalWrite(PinLed,LOW);}
 delay(50000);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+delay(1000); // wait for a second
 }
 
 // Plus la terre sera humide, plus la valeur s’approchera de 0, plus elle sera sèche, plus la valeur s’approchera de 4095.
