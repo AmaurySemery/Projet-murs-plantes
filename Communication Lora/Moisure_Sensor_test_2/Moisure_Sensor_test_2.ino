436bc53da9bed5a391ac9da5b8a377adbc937727
Amaury Semery <amaury.semery@laposte.net>
2020-09-07 10:29:13 +0200

    Update Moisure_Sensor_test_2.ino

--- Communication Lora/Moisure_Sensor_test_2/Moisure_Sensor_test_2.ino
+++ Communication Lora/Moisure_Sensor_test_2/Moisure_Sensor_test_2.ino
@@ -4,6 +4,8 @@
 #include <HTTPClient.h>
 #include "gravity_soil_moisture_sensor.h"
 
+#define USE_SERIAL Serial
+
 GravitySoilMoistureSensor gravity_sensor;
 WiFiMulti wifiMulti;
 
@@ -19,11 +21,20 @@ void setup() {
     uint8_t pin = 15;
 
     if (!gravity_sensor.Setup(pin)) {
-        Serial.println("Gravity Soil Moisture Sensor was not detected. Check wiring!");
+        Serial.println("Le capteur d'humidité au sol n'a pas été détecté.");
         while(1);
     } else {
-        Serial.println("Gravity Soil Moisture Sensor init done.");
+        Serial.println("Le capteur d'humidité au sol est opérationnel !");
     }
+Serial.println("\n");
+  WiFi.begin(ssid,password);  // Initialisation avec WiFi.begin / ssid et password
+  Serial.print("Attente de connexion ...");  // Message d'attente de connexion
+  while(WiFi.status() != WL_CONNECTED)  // Test connexion
+{Serial.print(".");  // Affiche des points .... tant que connexion n'est pas OK
+delay(100);}
+  Serial.println("\n");
+  Serial.println("Connexion établie !");  // Affiche connexion établie
+
 }
 
 void loop() {
@@ -39,6 +50,42 @@ Serial.println("% d'humidité.");
 if (sensorConvert1 < 35) {digitalWrite(PinLed,HIGH);}
 else {digitalWrite(PinLed,LOW);}
     
+int h = sensorConvert1;
 
-    delay(1000);
+if ((wifiMulti.run() == WL_CONNECTED)) { // Si c'est connecté, ça fait ce qu'il y a en dessous
+    HTTPClient http; // va créer un objet qui s'appelle HTTPClient qui va permettre de lancer des requêtes en HTTP
+    USE_SERIAL.println("[DEBG] " + String(h));
+
+    USE_SERIAL.print("[HTTP] begin...\n");
+    // configure traged server and url
+    //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+    http.begin("http://node03.popschool-willems.fr/#flow/b7090afb.bd8ce/H/" + String(h)); //HTTP => démarre connexion vers le serveur mentionné
+
+    USE_SERIAL.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET(); // soumet une requête de type "get", puis récupère résultat qui sera collée dans HTTP
+
+    // httpCode will be negative on error
+    if (httpCode > 0) { // si c'est supérieur à 0, il refait un test)
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) { // Si ça s'est bien passé, il refait une variable où il met le getString puis affiche à l'écran => on a reçu un code 200
+        String payload = http.getString();
+        USE_SERIAL.println(payload);
+        if (payload == "ON") {
+          digitalWrite(4, HIGH);
+        }
+        if (payload == "OFF") {
+          digitalWrite(4, LOW);
+        }
+      }
+    } else { // sinon, il dit que ça ne fonctionne pas
+      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();    }
+
+    delay(5000);
 }
