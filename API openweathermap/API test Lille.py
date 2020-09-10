7ff778829cea267e2198fd14097c793b821443d4
Amaury Semery <amaury.semery@laposte.net>
2020-09-10 10:18:40 +0200

    Update API test Lille.py

--- API openweathermap/API test Lille.py
+++ API openweathermap/API test Lille.py
@@ -1,6 +1,11 @@
 import requests
 import json
 from pyowm import OWM
+import config 
+import smopy 
+import matplotlib.pyplot as plt 
+
+owm = OWM('your-API-key')
 
 api_key = "7f9054c4edb12a97316c2da4d6fed6c9"
 lat = "50.633333"
@@ -10,3 +15,5 @@ url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&un
 response = requests.get(url)
 data = json.loads(response.text)
 print(data)
+
+baseurl='http://api.openweathermap.org/data/2.5/weather?appid='+config.apikey + "&units=metric"
