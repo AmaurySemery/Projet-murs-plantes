dd3adfacec20b7e0c0819d15e00dd7f44e192678
Amaury Semery <amaury.semery@laposte.net>
2020-09-10 14:43:13 +0200

    Update API test onecall.py

--- API openweathermap/Données/API test onecall.py
+++ API openweathermap/Données/API test onecall.py
@@ -1,9 +1,12 @@
 import requests
 import json
 
-https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&
-exclude=hourly,daily&appid={YOUR API KEY}
 join_key = "&appid=" + "9f9a2aa0b0d0871b54bda3bc56990983"
-OWM=https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689+join_key
+latlon = "lat=50.633333&lon=-94.037689"
+units = "&units=metric"
+key = "&appid=9f9a2aa0b0d0871b54bda3bc56990983"
+test = "https://api.openweathermap.org/data/2.5/onecall?"+latlon+key+units
+print(test)
 
-https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=9f9a2aa0b0d0871b54bda3bc56990983
+#https://api.openweathermap.org/data/2.5/onecall?lat=50.633333&lon=-94.037689&appid=9f9a2aa0b0d0871b54bda3bc56990983
+#https://api.openweathermap.org/data/2.5/onecall?lat=50.633333&%C4%BAon=-94.037689&appid=9f9a2aa0b0d0871b54bda3bc56990983&units=metric
