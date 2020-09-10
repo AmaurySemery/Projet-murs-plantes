c57ed67e618540d460ff43489bba4dccfdb92182
Amaury Semery <amaury.semery@laposte.net>
2020-09-10 12:02:30 +0200

    Create API test onecall.py

---
+++ API openweathermap/Données/API test onecall.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&
+exclude=hourly,daily&appid={YOUR API KEY}
+join_key = "&appid=" + "9f9a2aa0b0d0871b54bda3bc56990983"
+OWM=https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689+join_key
+
+https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=9f9a2aa0b0d0871b54bda3bc56990983
