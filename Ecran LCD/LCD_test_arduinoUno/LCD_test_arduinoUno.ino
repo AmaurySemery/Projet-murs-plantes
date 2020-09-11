a4461be5a97b55e29e0d36aeb4ddfe5be61d075b
Amaury Semery <amaury.semery@laposte.net>
2020-09-11 15:44:07 +0200

    Update LCD_test_arduinoUno.ino

--- Ecran LCD/LCD_test_arduinoUno/LCD_test_arduinoUno.ino
+++ Ecran LCD/LCD_test_arduinoUno/LCD_test_arduinoUno.ino
@@ -1,16 +1,3 @@
-* LCD RS pin to digital pin 12 
-* LCD Enable pin to digital pin 11 
-* LCD D4 pin to digital pin 5 
-* LCD D5 pin to digital pin 4 
-* LCD D6 pin to digital pin 3 
-* LCD D7 pin to digital pin 2 
-* LCD R/W pin to ground 
-* 10K resistor: * ends to +5V and ground 
-* wiper to LCD VO pin (pin 3) 
-This example code is in the public domain. 
-http://www.arduino.cc/en/Tutorial/LiquidCrystal */
-
-// on inclut la bibliothèque
 #include <LiquidCrystal.h>
 
 // définit un objet lcd avec les entrées sorties de l'Arduino en paramètre
