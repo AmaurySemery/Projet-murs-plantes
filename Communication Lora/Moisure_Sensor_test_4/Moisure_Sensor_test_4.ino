9859eaa9638be009135f541eff03f89acb7a2de4
Amaury Semery <amaury.semery@laposte.net>
2020-09-11 13:40:26 +0200

    Update Moisure_Sensor_test_4.ino

--- Communication Lora/Moisure_Sensor_test_4/Moisure_Sensor_test_4.ino
+++ Communication Lora/Moisure_Sensor_test_4/Moisure_Sensor_test_4.ino
@@ -14,6 +14,8 @@ HTTPClient http; // va créer un objet qui s'appelle HTTPClient qui va permettre
 const char* ssid = "POP_SENSORS";  // Mettre votre SSID Wifi
 const char* password = "P0PS3NS0RS!";  // Mettre votre mot de passe Wifi
 
+int buttonPin=15;
+int buttonState = 0;
 int PinLed1=2;
 int PinLed2=13;
 int PinLed3=14;
@@ -24,6 +26,7 @@ int sensorPin3 = 35;
 
 void setup() {
 
+  pinMode(buttonPin, INPUT);  
   pinMode(PinLed1,OUTPUT);
   pinMode(PinLed2,OUTPUT);
   pinMode(PinLed3,OUTPUT);
@@ -35,13 +38,13 @@ Serial.println("\n");
 //    } else {
 //        Serial.println("Le capteur d'humidité au sol est opérationnel !");
 //    }
-  WiFi.begin(ssid,password);  // Initialisation avec WiFi.begin / ssid et password
-  Serial.print("Attente de connexion ...");  // Message d'attente de connexion
-  while(WiFi.status() != WL_CONNECTED)  // Test connexion
-{Serial.print(".");  // Affiche des points .... tant que connexion n'est pas OK
-delay(100);}
-  Serial.println("\n");
-  Serial.println("Connexion établie !");  // Affiche connexion établie
+//  WiFi.begin(ssid,password);  // Initialisation avec WiFi.begin / ssid et password
+//  Serial.print("Attente de connexion ...");  // Message d'attente de connexion
+//  while(WiFi.status() != WL_CONNECTED)  // Test connexion
+//{Serial.print(".");  // Affiche des points .... tant que connexion n'est pas OK
+//delay(100);}
+//  Serial.println("\n");
+//  Serial.println("Connexion établie !");  // Affiche connexion établie
 
 }
 
@@ -79,42 +82,42 @@ if (sensor3Convert2 < 35) {digitalWrite(PinLed3,HIGH);}
 else {digitalWrite(PinLed3,LOW);}
 
 
-int h = sensorConvert1;
+//int h = sensorConvert1;
 
 
-if ((wifiMulti.run() == WL_CONNECTED)) { // Si c'est connecté, ça fait ce qu'il y a en dessous
-    USE_SERIAL.println("[DEBG] " + String(h));
+//if ((wifiMulti.run() == WL_CONNECTED)) { // Si c'est connecté, ça fait ce qu'il y a en dessous
+//    USE_SERIAL.println("[DEBG] " + String(h));
 
-    USE_SERIAL.print("[HTTP] begin...\n");
+//    USE_SERIAL.print("[HTTP] begin...\n");
     // configure traged server and url
     //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-    http.begin("https://192.168.1.200:1883/h/" + String(h)); //HTTP => démarre connexion vers le serveur mentionné
+//    http.begin("https://192.168.1.200:1883/h/" + String(h)); //HTTP => démarre connexion vers le serveur mentionné
 
-    USE_SERIAL.print("[HTTP] GET...\n");
+//    USE_SERIAL.print("[HTTP] GET...\n");
     // start connection and send HTTP header
-    int httpCode = http.GET(); // soumet une requête de type "get", puis récupère résultat qui sera collée dans HTTP
+//    int httpCode = http.GET(); // soumet une requête de type "get", puis récupère résultat qui sera collée dans HTTP
 
     // httpCode will be negative on error
-    if (httpCode > 0) { // si c'est supérieur à 0, il refait un test)
+//    if (httpCode > 0) { // si c'est supérieur à 0, il refait un test)
       // HTTP header has been send and Server response header has been handled
-      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+//      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
       // file found at server
-      if (httpCode == HTTP_CODE_OK) { // Si ça s'est bien passé, il refait une variable où il met le getString puis affiche à l'écran => on a reçu un code 200
-        String payload = http.getString();
-        USE_SERIAL.println(payload);
-        if (payload == "ON") {
-          digitalWrite(4, HIGH);
-        }
-        if (payload == "OFF") {
-          digitalWrite(4, LOW);
-        }
-      }
-    } else { // sinon, il dit que ça ne fonctionne pas
-      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
+//      if (httpCode == HTTP_CODE_OK) { // Si ça s'est bien passé, il refait une variable où il met le getString puis affiche à l'écran => on a reçu un code 200
+//        String payload = http.getString();
+//        USE_SERIAL.println(payload);
+//        if (payload == "ON") {
+//          digitalWrite(4, HIGH);
+//        }
+//        if (payload == "OFF") {
+//          digitalWrite(4, LOW);
+//        }
+//      }
+//    } else { // sinon, il dit que ça ne fonctionne pas
+//      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+//    }
 
-    http.end();    }
+//    http.end();    }
 
     delay(5000);
 }
