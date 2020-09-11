96b4fa5034fd6470ad1d97127d4a509e946903c3
Amaury Semery <amaury.semery@laposte.net>
2020-09-11 10:59:37 +0200

    1

--- API openweathermap/API test onecall.py
+++ API openweathermap/API test onecall.py
@@ -7,7 +7,17 @@ lon = "3.066667"
 units = "&units=metric"
 key = "&appid=9f9a2aa0b0d0871b54bda3bc56990983"
 langage = "&lang=fr"
-test = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+key+units+langage
-print(test)
+url = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+key+units+langage
+print(url)
 
-list=[]
+response = requests.get(url)
+data = json.loads(response.text)
+
+with open("data_file.json", "w") as write_file:
+
+    json.dump(data, write_file)
+
+#list=[]
+
+#for line in data:
+#    tempmax,tempmin,
