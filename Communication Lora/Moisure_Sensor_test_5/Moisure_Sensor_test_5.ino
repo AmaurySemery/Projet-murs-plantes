e76a5f8a7a727fd5b66a561f1f8bd003410a404f
Amaury Semery <amaury.semery@laposte.net>
2020-09-14 15:51:54 +0200

    Update Moisure_Sensor_test_5.ino

--- Communication Lora/Moisure_Sensor_test_5/Moisure_Sensor_test_5.ino
+++ Communication Lora/Moisure_Sensor_test_5/Moisure_Sensor_test_5.ino
@@ -39,7 +39,7 @@ void setup() {
 //lcd.setCursor(0,1);
 //lcd.print("LCD with ESP32");
 //  pinMode(buttonPin, INPUT_PULLUP);  
-  pinMode(PinLed,OUTPUT);
+//  pinMode(PinLed,OUTPUT);
   Serial.begin(115200);
 Serial.println("Bienvenue sur le programme Moisture Sensor !");
 Serial.println("**********");
