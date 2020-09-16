310d9e95bbae281474a7d9a76ba33b9d479a390f
Amaury Semery <amaury.semery@laposte.net>
2020-09-16 17:11:59 +0200

    Update Gestion données capteurs.py

--- Python/Gestion données capteurs.py
+++ Python/Gestion données capteurs.py
@@ -1,4 +1,4 @@
 import json
 
-with open("/home/popschool/Documents/GitHub/solioti/Python/Données capteurs/capteurs-json.txt", "r") as read_file:
+with open("/home/popschool/Documents/GitHub/solioti/Python/Données capteurs/capteurs-brut.txt", "r") as read_file:
     data = json.load(read_file)
