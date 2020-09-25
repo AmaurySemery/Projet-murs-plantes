a6414a1ccf3d0ce0091c2e8532b285db3807369f
Amaury Semery <amaury.semery@laposte.net>
2020-09-25 14:15:59 +0200

    Update Module_cuve.ino

--- Module cuve/Module_cuve/Module_cuve.ino
+++ Module cuve/Module_cuve/Module_cuve.ino
@@ -3,6 +3,10 @@
 // defines pins numbers / definition des broches du capteur
 const int trigPin = 13;
 const int echoPin = 2;
+int maDistance = 0;
+int hauteur_maxi = 10;
+int hauteur_restante;
+float pourcentage;
  
 // Initialize sensor that uses digital pins trigPin and echoPin / initialisation du capteur avec les broches utilisees.
 UltraSonicDistanceSensor distanceSensor(trigPin, echoPin);
@@ -12,6 +16,17 @@ void setup() {
 }
 void loop() {
   // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters./ toutes les 500 millisecondes nous faisons une mesure et nous affichons la distance en centimetre sur le port serie.
-  Serial.println(distanceSensor.measureDistanceCm());
+  maDistance = distanceSensor.measureDistanceCm();
+  Serial.println(maDistance);
+  hauteur_restante = hauteur_maxi - maDistance;
+
+ Serial.print("hauteur restante : ");
+ Serial.println( hauteur_restante);
+
+pourcentage = (hauteur_restante * 100) / hauteur_maxi;
+
+Serial.print("pourcentage : ");
+Serial.print( pourcentage, 2);
+Serial.println(" %");
   delay(5000);
 }
