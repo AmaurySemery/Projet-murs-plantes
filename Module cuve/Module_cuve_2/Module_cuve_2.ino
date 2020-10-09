edbca3b4a7f8e9a25b35ffdf87e633c47db1721a
Amaury Semery <amaury.semery@laposte.net>
2020-10-09 14:49:35 +0200

    0

--- Module cuve/Module_cuve_2/Module_cuve_2.ino
+++ Module cuve/Module_cuve_2/Module_cuve_2.ino
@@ -2,68 +2,24 @@
 #include <WiFi.h>  // Utilisation de la librairie WiFi.h
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h> 
-//#include <WiFiMulti.h>
-//#include <HTTPClient.h>
 #include <Arduino.h>
 
 //#define USE_SERIAL Serial
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
-// defines pins numbers / definition des broches du capteur
-int led1 = 23;
-int led2 = 4;
-int led3 = 22;
-int led4 = 21;
-const int trigPin = 13;
-const int echoPin = 2;
-int maDistance = 0;
-int hauteur_maxi = 10;
-int hauteur_restante;
-float pourcentage;
-//WiFiMulti wifiMulti;
-const char* ssid = "POP_SENSORS";  // Mettre votre SSID Wifi
-const char* password = "P0PS3NS0RS!";  // Mettre votre mot de passe Wifi
- 
-// Initialize sensor that uses digital pins trigPin and echoPin / initialisation du capteur avec les broches utilisees.
-UltraSonicDistanceSensor distanceSensor(trigPin, echoPin);
+
 void setup() {
 
-lcd.init();                      // initialize the lcd 
-  // Print a message to the LCD.
-  lcd.backlight();
-  lcd.setCursor(3,0);
+lcd.init();
+ lcd.backlight();
+  lcd.setCursor(1,0);
   lcd.print("Hello, world!");
-  lcd.setCursor(2,1);
-  lcd.print("Ywrobot Arduino!");
-   lcd.setCursor(0,2);
-  lcd.print("Arduino LCM IIC 2004");
-   lcd.setCursor(2,3);
-  lcd.print("Power By Ec-yuan!");
-  
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
-  pinMode(led4,OUTPUT);
-  // We initialize serial connection so that we could print values from sensor./ initialisation du port serie a 115200 band pour afficher les valeurs mesurees par le capteur.
-  Serial.begin(115200);
-//  wifiMulti.addAP(ssid, password);
+  delay(5000);
+
 }
 void loop() {
-  // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters./ toutes les 500 millisecondes nous faisons une mesure et nous affichons la distance en centimetre sur le port serie.
-  maDistance = distanceSensor.measureDistanceCm();
-  Serial.println(maDistance);
-  hauteur_restante = hauteur_maxi - maDistance;
+lcd.clear(); 
+lcd.setCursor(0,0); // positionne le curseur à la colonne 1 et à la ligne 2 lcd.print("Hello World!"); delay(1000);
 
- Serial.print("Contenu : ");
- Serial.print( hauteur_restante);
- Serial.println(" cm.");
-
-pourcentage = (hauteur_restante * 100) / hauteur_maxi;
-
-Serial.print("Niveau de remplissage de la cuve : ");
-Serial.print( pourcentage, 2);
-Serial.println(" %");
-
-  delay(5000);
 }
