4f878f6a6391f180205820e716bcabd3d4405ee5
Amaury Semery <amaury.semery@laposte.net>
2020-10-09 16:16:48 +0200

    Update Module_cuve_2.ino

--- Module cuve/Module_cuve_2/Module_cuve_2.ino
+++ Module cuve/Module_cuve_2/Module_cuve_2.ino
@@ -6,7 +6,7 @@
 
 //#define USE_SERIAL Serial
 
-LiquidCrystal_I2C lcd(0x2F,16,2);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 // Le capteur SR04M a une portée de 20cm à 8m
 
@@ -22,20 +22,20 @@ UltraSonicDistanceSensor distanceSensor(trigPin, echoPin);
 void setup() {
 lcd.init();
  lcd.backlight();
-  lcd.setCursor(1,0);
-  lcd.print("Lancement du");
+  lcd.setCursor(0,0);
+  lcd.print("Module cuve...");
   lcd.setCursor(0,1);
-  lcd.print("programme !");
+  lcd.print("Activation !");
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
     Serial.begin(115200);
-
+delay(5000);
 
 }
-void loop() {maDistance = distanceSensor.measureDistanceCm();
+void loop() {
+    maDistance = distanceSensor.measureDistanceCm();
   Serial.println(maDistance);
   hauteur_restante = hauteur_maxi - maDistance;
-
  Serial.print("Contenu : ");
  Serial.print( hauteur_restante);
  Serial.println(" cm.");
@@ -44,5 +44,12 @@ pourcentage = (hauteur_restante * 100) / hauteur_maxi;
 
 Serial.print("Niveau de remplissage de la cuve : ");
 Serial.print( pourcentage, 2);
+lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(maDistance);
+  lcd.print(" cm");
+  lcd.setCursor(0,1);
+  lcd.print(pourcentage);
+  lcd.print(" %");
 delay(5000);
 }
