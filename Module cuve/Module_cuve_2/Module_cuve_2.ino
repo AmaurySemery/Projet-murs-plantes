755c7b417a9a73dfc989e7163740bb235ddf531c
Amaury Semery <amaury.semery@laposte.net>
2020-10-09 15:37:34 +0200

    Update Module_cuve_2.ino

--- Module cuve/Module_cuve_2/Module_cuve_2.ino
+++ Module cuve/Module_cuve_2/Module_cuve_2.ino
@@ -1,21 +1,32 @@
-#include <HCSR04.h>      
+#include <HCSR04.h>
+#include <NewPing.h>   
 #include <WiFi.h>  // Utilisation de la librairie WiFi.h
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h> 
 #include <Arduino.h>
 
 //#define USE_SERIAL Serial
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 LiquidCrystal_I2C lcd(0x2F,16,2);
 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
-
 lcd.init();
  lcd.backlight();
   lcd.setCursor(1,0);
-  lcd.print("Hello, world!");
+  lcd.print("Module cuve");
+    Serial.begin(115200);
+
 
 }
 void loop() {
+  
+ delay(5000);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
 }
