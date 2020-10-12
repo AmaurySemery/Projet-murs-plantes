4cb69d6e38edca20d4066ca18229feab1da2881d
Amaury Semery <amaury.semery@laposte.net>
2020-10-12 10:36:22 +0200

    Update humidity_v2_test_1.ino

--- Capteur humidité sol/humidity_v2_test_1/humidity_v2_test_1.ino
+++ Capteur humidité sol/humidity_v2_test_1/humidity_v2_test_1.ino
@@ -5,6 +5,7 @@ GravitySoilMoistureSensor gravity_sensor;
 
 int sensorPin1 = A0;
 int sensorPin2 = A1;
+int sensorPin3 = A3;
 
 
 void setup(){
@@ -15,10 +16,13 @@ void loop() {
  //Read YL-69 value
    uint16_t value1 = analogRead(sensorPin1);
    uint16_t value2 = analogRead(sensorPin2);
+   uint16_t value3 = analogRead(sensorPin3);
 
 Serial.print("Capteur 1 : ");
 Serial.println(value1); 
 Serial.print("Capteur 2 : ");
 Serial.println(value2);
+Serial.print("Capteur 3 : ");
+Serial.println(value3);
 delay(5000);
 }
