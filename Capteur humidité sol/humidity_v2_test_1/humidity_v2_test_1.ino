c0cd88ae0e972db5fd9038c9169825596c6185b6
Amaury Semery <amaury.semery@laposte.net>
2020-10-12 10:30:03 +0200

    Update humidity_v2_test_1.ino

--- Capteur humidité sol/humidity_v2_test_1/humidity_v2_test_1.ino
+++ Capteur humidité sol/humidity_v2_test_1/humidity_v2_test_1.ino
@@ -1,4 +1,11 @@
-#define wait 5000
+#include <Arduino.h>
+#include "gravity_soil_moisture_sensor.h"
+
+GravitySoilMoistureSensor gravity_sensor;
+
+int sensorPin1 = A0;
+int sensorPin2 = A1;
+
 
 void setup(){
   Serial.begin(115200);
@@ -6,10 +13,12 @@ void setup(){
 
 void loop() {
  //Read YL-69 value
- int a5 = analogRead(2); 
- 
- Serial.print("{\"YL69\":");
- Serial.print(a5);
- Serial.println("}");
- delay(wait);
+   uint16_t value1 = analogRead(sensorPin1);
+   uint16_t value1 = analogRead(sensorPin2);
+
+Serial.print("Capteur 1 : ");
+Serial.println(sensorPin1); 
+Serial.print("Capteur 2 : ");
+Serial.println(sensorPin2);
+delay(5000);
 }
