81a9e2b897f4d4ec0af94092ee06ad4131002f53
Amaury Semery <amaury.semery@laposte.net>
2020-10-12 15:32:17 +0200

    Update Moisure_Sensor_esp32.ino

--- Communication Lora/Moisure_Sensor_esp32/Moisure_Sensor_esp32.ino
+++ Communication Lora/Moisure_Sensor_esp32/Moisure_Sensor_esp32.ino
@@ -82,7 +82,7 @@ void setup() {
     lcd.clear();
   }
 
-  wifiMulti.addAP(ssid, password);
+//  wifiMulti.addAP(ssid, password);
 
   lcd.clear();
   lcd.setCursor(0,0);
@@ -137,49 +137,49 @@ lcd.clear();
   int moy = moyenne;
 
 
-  if ((wifiMulti.run() == WL_CONNECTED)) { // Si c'est connecté, ça fait ce qu'il y a en dessous
+//  if ((wifiMulti.run() == WL_CONNECTED)) { // Si c'est connecté, ça fait ce qu'il y a en dessous
 
-    HTTPClient http; // va créer un objet qui s'appelle HTTPClient qui va permettre de lancer des requêtes en HTTP
+//    HTTPClient http; // va créer un objet qui s'appelle HTTPClient qui va permettre de lancer des requêtes en HTTP
 
-    USE_SERIAL.println("[DEBG] " + String(sen1) + String(sen2) + String(sen3) + String(mi) + String(ma) + String(al) + String(moy));
+//    USE_SERIAL.println("[DEBG] " + String(sen1) + String(sen2) + String(sen3) + String(mi) + String(ma) + String(al) + String(moy));
 
-    USE_SERIAL.print("[HTTP] begin...\n");
+//    USE_SERIAL.print("[HTTP] begin...\n");
     // configure traged server and url
     //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
 
-    http.begin("http://node03.popschool-willems.fr:1880/mod1/" + String(sen1)+ "/" + String(sen2) + "/" +String(sen3) ); //HTTP => démarre connexion vers le serveur mentionné/
+//    http.begin("http://node03.popschool-willems.fr:1880/mod1/" + String(sen1)+ "/" + String(sen2) + "/" +String(sen3) ); //HTTP => démarre connexion vers le serveur mentionné/
 
-    USE_SERIAL.print("[HTTP] GET...\n");
+//    USE_SERIAL.print("[HTTP] GET...\n");
     // start connection and send HTTP header
-    int httpCode = http.GET(); // soumet une requête de type "get", puis récupère résultat qui sera collée dans HTTP
+//    int httpCode = http.GET(); // soumet une requête de type "get", puis récupère résultat qui sera collée dans HTTP
 
     // httpCode will be negative on error
-    if (httpCode > 0) { // si c'est supérieur à 0, il refait un test)
+//    if (httpCode > 0) { // si c'est supérieur à 0, il refait un test)
       // HTTP header has been send and Server response header has been handled
-      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+//      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
       // file found at server
-      if (httpCode == HTTP_CODE_OK) { // Si ça s'est bien passé, il refait une variable où il met le getString puis affiche à l'écran => on a reçu un code 200
-        String payload = http.getString();
-        USE_SERIAL.println(payload);
-      lcd.print("Retour HTTP :");
-      lcd.setCursor(0, 1);
-      lcd.print(payload);
-        if (payload == "50") {
-          USE_SERIAL.println("Coucou");
-        }
-        if (payload == "OFF") {
-          digitalWrite(4, LOW);
-        }
-      }
-    } else { // sinon, il dit que ça ne fonctionne pas
-      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      lcd.print("Erreur :");
-      lcd.setCursor(0, 1);
-      lcd.print("Requete HTTP");
-    }
-    http.end();
-  }
+//      if (httpCode == HTTP_CODE_OK) { // Si ça s'est bien passé, il refait une variable où il met le getString puis affiche à l'écran => on a reçu un code 200
+//        String payload = http.getString();
+//        USE_SERIAL.println(payload);
+//      lcd.print("Retour HTTP :");
+//      lcd.setCursor(0, 1);
+//      lcd.print(payload);
+//        if (payload == "50") {
+//          USE_SERIAL.println("Coucou");
+//        }
+//        if (payload == "OFF") {
+//          digitalWrite(4, LOW);
+//        }
+//      }
+//    } else { // sinon, il dit que ça ne fonctionne pas
+//      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+//      lcd.print("Erreur :");
+//      lcd.setCursor(0, 1);
+//      lcd.print("Requete HTTP");
+//    }
+//    http.end();
+//  }
   delay(mn10);
   lcd.clear();
   
