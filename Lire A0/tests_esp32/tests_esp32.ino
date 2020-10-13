60d797b1f40d308c77d21abf76a96b736a483257
Amaury Semery <amaury.semery@laposte.net>
2020-10-13 13:37:59 +0200

    Update tests_esp32.ino

--- Lire A0/tests_esp32/tests_esp32.ino
+++ Lire A0/tests_esp32/tests_esp32.ino
@@ -7,6 +7,8 @@ GravitySoilMoistureSensor gravity_sensor;
 int sensorPin1 = 13;
 int sensorPin2 = 27;
 int sensorPin3 = 14;
+float a = 40.95;
+int b = 100;
 
 
 void setup() {
@@ -15,14 +17,24 @@ void setup() {
 
 void loop() {
   uint16_t value1 = analogRead(sensorPin1);
+  float division1 = value1 / a;
+  int sensor1convert = b - division1;
+  Serial.println("Pourcentage d'humidité du mur végétalisé :");
   Serial.print("Capteur 1 : ");
-  Serial.println(value1);
+  Serial.print(sensor1convert);
+  Serial.println(" %");
   uint16_t value2 = analogRead(sensorPin2);
+  float division2 = value2 / a;
+  int sensor2convert = b - division2;
   Serial.print("Capteur 2 : ");
-  Serial.println(value2);
+  Serial.print(sensor2convert);
+  Serial.println(" %");
   uint16_t value3 = analogRead(sensorPin3);
-  Serial.print("Capteur 3 : ");
-  Serial.println(value3);
+  float division3 = value3 / a;
+  int sensor3convert = b - division3;
+  Serial.print("Capteur 2 : ");
+  Serial.print(sensor3convert);
+  Serial.println(" %");
   Serial.println("----------------");
 
   delay(5000);}
