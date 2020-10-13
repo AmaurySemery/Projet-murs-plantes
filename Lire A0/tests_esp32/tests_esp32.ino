93637714abfd61a09d390c713a03fe5f61947b10
Amaury Semery <amaury.semery@laposte.net>
2020-10-13 11:59:25 +0200

    0

---
+++ Lire A0/tests_esp32/tests_esp32.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "gravity_soil_moisture_sensor.h"
+
+
+GravitySoilMoistureSensor gravity_sensor;
+
+int sensorPin1 = 13;
+int sensorPin2 = 12;
+
+void setup() {
+  Serial.begin(115200);
+  }
+
+void loop() {
+  uint16_t value1 = analogRead(sensorPin1);
+  Serial.print("Capteur 1 : ");
+  Serial.println(value1);
+  uint16_t value2 = analogRead(sensorPin2);
+  Serial.print("Capteur 2 : ");
+  Serial.println(value2);
+  Serial.println("------------------");
+
+  delay(5000);}
