072d150ab7767b9629ddd03a8b0d6705cd93d94e
Amaury Semery <amaury.semery@laposte.net>
2020-10-13 10:59:25 +0200

    Update tests_wemox.ino

--- Lire A0/tests_wemox/tests_wemox.ino
+++ Lire A0/tests_wemox/tests_wemox.ino
@@ -8,3 +8,27 @@
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 GravitySoilMoistureSensor gravity_sensor;
+
+int sensorPin1 = 2;
+
+void setup() {
+  Serial.begin(115200);
+  if (!gravity_sensor.Setup(sensorPin1)) {
+  Serial.println("Le capteur 1 d'humidité au sol n'a pas été détecté.");
+    lcd.print("Capteur 1 : Err !");
+    delay(3000);
+    lcd.clear();
+}
+else {
+    Serial.println("Le capteur 1 d'humidité au sol est opérationnel !");
+    lcd.print("Capteur 1 : Ok !");
+    delay(3000);
+    lcd.clear();
+  }
+  }
+
+void loop() {
+  uint16_t value1 = analogRead(sensorPin1);
+  lcd.print(value1);
+  delay(5000);
+  lcd.clear();}
