1f86588c1fdba7d0720e9b0a74d9e717555f2cbb
Amaury Semery <amaury.semery@laposte.net>
2020-10-13 14:04:11 +0200

    Update tests_esp32.ino

--- Lire A0/tests_esp32/tests_esp32.ino
+++ Lire A0/tests_esp32/tests_esp32.ino
@@ -1,14 +1,22 @@
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
 
+WiFiMulti wifiMulti;
 
-int sensorPin1 = 13;
-int sensorPin2 = 27;
-int sensorPin3 = 14;
+const char* ssid = "POP_SENSORS";
+const char* password = "P0PS3NS0RS!";
+
+int sensorPin1 = 32;
+int sensorPin2 = 34;
+int sensorPin3 = 35;
 float a = 40.95;
 int b = 100;
 
 
 void setup() {
   Serial.begin(115200);
+wifiMulti.addAP(ssid, password);
   }
 
 void loop() {
@@ -32,5 +40,35 @@ void loop() {
   Serial.print(sensor3convert);
   Serial.println(" %");
   Serial.println("----------------");
+
+    if((wifiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+
+        Serial.print("[HTTP] begin...\n");
+        // configure traged server and url
+        //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+        http.begin("http://example.com/index.html"); //HTTP
+
+        Serial.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http.GET();
+
+        // httpCode will be negative on error
+        if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                Serial.println(payload);
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
 
   delay(5000);}
