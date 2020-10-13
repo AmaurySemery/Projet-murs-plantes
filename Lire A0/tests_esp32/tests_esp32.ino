0804e08fb9ba0b336f581f3bab04f1967ec77844
Amaury Semery <amaury.semery@laposte.net>
2020-10-13 12:10:10 +0200

    Update tests_esp32.ino

--- Lire A0/tests_esp32/tests_esp32.ino
+++ Lire A0/tests_esp32/tests_esp32.ino
@@ -5,6 +5,9 @@
 GravitySoilMoistureSensor gravity_sensor;
 
 int sensorPin1 = 13;
+int sensorPin2 = 12;
+int sensorPin3 = 14;
+
 
 void setup() {
   Serial.begin(115200);
@@ -14,5 +17,11 @@ void loop() {
   uint16_t value1 = analogRead(sensorPin1);
   Serial.print("Capteur 1 : ");
   Serial.println(value1);
+  uint16_t value2 = analogRead(sensorPin2);
+  Serial.print("Capteur 1 : ");
+  Serial.println(value2);
+  uint16_t value3 = analogRead(sensorPin3);
+  Serial.print("Capteur 1 : ");
+  Serial.println(value3);
 
   delay(5000);}
