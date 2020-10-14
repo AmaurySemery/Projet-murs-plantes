0cd4a9b20d0c4e87d432ccf81aa337e3b4511ba7
Amaury Semery <amaury.semery@laposte.net>
2020-10-14 11:09:16 +0200

    Update module_cuve_tbeam_final.ino

--- Module cuve/module_cuve_tbeam_final/module_cuve_tbeam_final.ino
+++ Module cuve/module_cuve_tbeam_final/module_cuve_tbeam_final.ino
@@ -1,18 +1,28 @@
 
 #include <UltrasonicSensor.h>
+#include <WiFi.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 UltrasonicSensor ultrasonic(0, 4);
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+int hauteur_maxi = 100; // modifier suivant la hauteur de la cuve
 
 void setup() {
   Serial.begin(115200);
-  
 }
 
 void loop() {
   int distance = ultrasonic.distanceInCentimeters();
-  
+  int hauteur_restante = hauteur_maxi - distance;
+  int pourcentage = (distance * 100) / hauteur_maxi;
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+  Serial.print("Niveau de remplissage de la cuve : ");
+  Serial.print(pourcentage);
+  Serial.println(" %");
+  Serial.println("-----------------");
   delay(5000);
 }
