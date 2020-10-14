ff7b31ed17b4fd093f7350f2c5213e246bdd3a38
Amaury Semery <amaury.semery@laposte.net>
2020-10-14 11:30:19 +0200

    Update module_cuve_tbeam_final.ino

--- Module cuve/module_cuve_tbeam_final/module_cuve_tbeam_final.ino
+++ Module cuve/module_cuve_tbeam_final/module_cuve_tbeam_final.ino
@@ -1,16 +1,22 @@
-
 #include <UltrasonicSensor.h>
 #include <WiFi.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
 
 UltrasonicSensor ultrasonic(0, 4);
 LiquidCrystal_I2C lcd(0x27,16,2);
 
+WiFiMulti wifiMulti;
+
 int hauteur_maxi = 100; // modifier suivant la hauteur de la cuve
+const char* ssid = "POP_SENSORS";
+const char* password = "P0PS3NS0RS!";
 
 void setup() {
   Serial.begin(115200);
+  wifiMulti.addAP(ssid, password);
 }
 
 void loop() {
@@ -27,5 +33,21 @@ void loop() {
   Serial.print(pourcentage);
   Serial.println(" %");
   Serial.println("-----------------");
+
+if ((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    Serial.println("[DEBG] " + String(distance) + String(hauteur_restante) + String(pourcentage));
+    http.begin("http://node03.popschool-willems.fr:1880/mod2/" + String(pourcentage));
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);}}
+    else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();}
+  
   delay(5000);
 }
