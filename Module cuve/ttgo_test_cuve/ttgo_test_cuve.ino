17b6183261a92162385c94a50724aadfe91a001e
Amaury Semery <amaury.semery@laposte.net>
2020-10-14 10:43:51 +0200

    Update ttgo_test_cuve.ino

--- Module cuve/ttgo_test_cuve/ttgo_test_cuve.ino
+++ Module cuve/ttgo_test_cuve/ttgo_test_cuve.ino
@@ -1,21 +1,18 @@
 
-#define ECHOPIN 0// Pin to receive echo pulse
-#define TRIGPIN 4// Pin to send trigger pulse
-void setup(){
+#include <UltrasonicSensor.h>
+
+UltrasonicSensor ultrasonic(0, 4);
+
+void setup() {
   Serial.begin(115200);
-  pinMode(ECHOPIN, INPUT);
-  pinMode(TRIGPIN, OUTPUT);
-  digitalWrite(ECHOPIN, HIGH);
+  
 }
-void loop(){
-  digitalWrite(TRIGPIN, LOW); // Set the trigger pin to low for 2uS
-  delayMicroseconds(2);
-  digitalWrite(TRIGPIN, HIGH); // Send a 10uS high to trigger ranging
-  delayMicroseconds(10);
-  digitalWrite(TRIGPIN, LOW); // Send pin low again
-  int distance = pulseIn(ECHOPIN, HIGH,26000); // Read in times pulse
-  distance= distance/58;
+
+void loop() {
+  int distance = ultrasonic.distanceInCentimeters();
+  
+  Serial.print("Distance: ");
   Serial.print(distance);
-  Serial.println("   cm");
-  delay(50);// Wait 50mS before next ranging
+  Serial.println(" cm");
+  delay(5000);
 }
