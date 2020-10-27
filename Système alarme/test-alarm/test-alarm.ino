4bcc8c762377d2ffa5aef614d07ed06912c63e6e
Amaury Semery <amaury.semery@laposte.net>
2020-10-27 14:12:11 +0100

    Update test-alarm.ino

--- Système alarme/test-alarm/test-alarm.ino
+++ Système alarme/test-alarm/test-alarm.ino
@@ -2,30 +2,49 @@ int tiger=2;
 int echo=3;
 int a=12;
 int Duration,Distance;
-void setup()
-{
-  pinMode(tiger,OUTPUT);
-  pinMode(echo,INPUT);
-  Serial.begin(9600);
-  pinMode(a,OUTPUT);
-}
-void loop()
-{
-  digitalWrite(tiger,HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(tiger,LOW);
-  Duration=pulseIn(echo,HIGH);
-  Distance=(Duration/2)/29.1;
-  Serial.print(Distance);
-  Serial.println("cm");
-  delay(5000);
-  if (Distance<50)
-  {
-   digitalWrite(a,HIGH);
-   delay(1000);
-    }
- else {
- digitalWrite(a,LOW);
-  delay(200);}
+int redpin = 11; //select the pin for the red LED
+int bluepin =10; // select the pin for the  blue LED
+int greenpin = 9;// select the pin for the green LED
+
+void setup(){
+   pinMode(tiger,OUTPUT);
+   pinMode(echo,INPUT);
+   Serial.begin(9600);
+   pinMode(a,OUTPUT);
+   pinMode(redpin, OUTPUT);
+   pinMode(bluepin, OUTPUT);
+   pinMode(greenpin, OUTPUT);}
+
+void loop(){
+   digitalWrite(tiger,HIGH);
+   delayMicroseconds(1000);
+   digitalWrite(tiger,LOW);
+   Duration=pulseIn(echo,HIGH);
+   Distance=(Duration/2)/29.1;
   
-}
+if (Distance<10){
+   digitalWrite(a,HIGH);
+   digitalWrite(redpin,HIGH);
+   digitalWrite(bluepin,LOW);
+   digitalWrite(greenpin,LOW);
+   Serial.print(Distance);
+   Serial.println("cm");
+   delay(200);}
+    
+if (Distance>10 and Distance<20){
+   digitalWrite(greenpin,HIGH);
+   digitalWrite(redpin,LOW);
+   digitalWrite(bluepin,LOW);
+   Serial.print(Distance);
+   Serial.println("cm");
+   delay(200);}
+  
+if (Distance>20) {
+   digitalWrite(bluepin,HIGH);
+   digitalWrite(redpin,LOW);
+   digitalWrite(greenpin,LOW);
+   Serial.print(Distance);
+   Serial.println("cm");
+   delay(200);}
+
+   delay(5000);}
