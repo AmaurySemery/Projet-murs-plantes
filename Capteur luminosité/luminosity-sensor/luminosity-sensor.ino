76cc88a5916ec521345ebb425048b06c596a4f14
Amaury Semery <amaury.semery@laposte.net>
2020-10-27 13:09:27 +0100

    0

--- Capteur luminosité/luminosity-sensor/luminosity-sensor.ino
+++ Capteur luminosité/luminosity-sensor/luminosity-sensor.ino
@@ -1,9 +1,13 @@
 int sensorPin = 34; //define analog pin 2
-int value = 0; 
+int value = 0;
+int alerte = 32;
+
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Lancement du programme !");
+  pinMode(alerte,OUTPUT);
+
 } 
 
 void loop() {
@@ -11,5 +15,17 @@ void loop() {
   Serial.println(value, DEC); // light intensity
                 // high values for bright environment
                 // low values for dark environment
+  if (value<1400)
+  {
+   digitalWrite(alerte,HIGH);
+   Serial.println("Alerte : on");
+   delay(2000);
+    }
+ else {
+ digitalWrite(alerte,LOW);
+ Serial.println("Alerte : off");
+ delay(2000);}
+
+
   delay(5000); 
 }
