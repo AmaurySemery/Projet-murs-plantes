07e4fa8e015788541866cb0e0f5d133778540179
Amaury Semery <amaury.semery@laposte.net>
2020-10-28 14:38:06 +0100

    Update module_cuve_tbeam_final.ino

--- Module cuve/module_cuve_tbeam_final/module_cuve_tbeam_final.ino
+++ Module cuve/module_cuve_tbeam_final/module_cuve_tbeam_final.ino
@@ -17,40 +17,40 @@ const char* password = "P0PS3NS0RS!";
 
 void setup() {
   Serial.begin(115200);
-//  wifiMulti.addAP(ssid, password);
+  wifiMulti.addAP(ssid, password);
 }
 
 void loop() {
   int distance = ultrasonic.distanceInCentimeters();
   int hauteur_restante = hauteur_totale - distance;
-  int hauteur_restante_cuve = hauteur_cuve - hauteur_restante;
-  Serial.println(hauteur_restante_cuve);
+  int hauteur_perdue = hauteur_cuve - hauteur_restante;
+  int hauteur_restante_cuve = hauteur_cuve - hauteur_perdue;
   int pourcentage = (hauteur_restante_cuve * 100) / hauteur_cuve;
   Serial.print("Distance capteur/surface: ");
   Serial.print(distance);
   Serial.println(" cm");
   Serial.print("Niveau de liquide restant estimé dans la cuve : ");
-  Serial.print(hauteur_restante);
+  Serial.print(hauteur_restante_cuve);
   Serial.println(" cm");
   Serial.print("Niveau de remplissage de la cuve : ");
   Serial.print(pourcentage);
   Serial.println(" %");
   Serial.println("-----------------");
 
-//if ((wifiMulti.run() == WL_CONNECTED)) {
-//    HTTPClient http;
-//    Serial.println("[DEBG] " + String(distance) + String(hauteur_restante) + String(pourcentage));
-//    http.begin("http://node03.popschool-willems.fr:1880/mod2/" + String(pourcentage));
-//    int httpCode = http.GET();
-//    if (httpCode > 0) {
-//      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-//      if (httpCode == HTTP_CODE_OK) {
-//        String payload = http.getString();
-//        Serial.println(payload);}}
-//    else {
-//      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-//    }
-//    http.end();}
+if ((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    Serial.println("[DEBG] " + String(distance) + String(hauteur_restante) + String(pourcentage));
+    http.begin("http://node03.popschool-willems.fr:1880/mod2/" + String(pourcentage));
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);}}
+    else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();}
   
   delay(5000);
 }
