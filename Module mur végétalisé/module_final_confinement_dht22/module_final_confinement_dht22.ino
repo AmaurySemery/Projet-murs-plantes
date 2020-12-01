d2c81705997ca4b20c2130a8318b11a65267dfbc
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-12-01 14:49:33 +0100

    Update module_final_confinement_dht22.ino

--- Module mur végétalisé/module_final_confinement_dht22/module_final_confinement_dht22.ino
+++ Module mur végétalisé/module_final_confinement_dht22/module_final_confinement_dht22.ino
@@ -14,9 +14,7 @@ WiFiMulti wifiMulti;
 const char* ssid = "freebox_IPLMWO";
 const char* password = "F4CAE54A9B34";
 
-int sensorPin1 = 34;
-int sensorPin2 = 32;
-int sensorPin3 = 35;
+int sensorPin = 34;
 float a = 40.95;
 int b = 100;
 
@@ -42,28 +40,16 @@ void loop() {
     Serial.println(F("°C"));
   }
   
-  uint16_t value1 = analogRead(sensorPin1);
-  float division1 = value1 / a;
-  int sensor1convert = b - division1;
+  uint16_t value = analogRead(sensorPin);
+  float division = value / a;
+  int sensorconvert = b - division;
   Serial.println("Pourcentage d'humidité du mur végétalisé :");
-  Serial.print("Capteur 1 : ");
-  Serial.print(sensor1convert);
-  Serial.println(" %");
-  uint16_t value2 = analogRead(sensorPin2);
-  float division2 = value2 / a;
-  int sensor2convert = b - division2;
-  Serial.print("Capteur 2 : ");
-  Serial.print(sensor2convert);
-  Serial.println(" %");
-  uint16_t value3 = analogRead(sensorPin3);
-  float division3 = value3 / a;
-  int sensor3convert = b - division3;
-  Serial.print("Capteur 3 : ");
-  Serial.print(sensor3convert);
+  Serial.print("Capteur : ");
+  Serial.print(sensorconvert);
   Serial.println(" %");
   Serial.println("----------------");
 
-  int sen1 = sensor1convert;
+  int sen = sensorconvert;
   int sen2 = sensor2convert;
   int sen3 = sensor3convert;
   int temp = event.temperature;
@@ -72,8 +58,8 @@ void loop() {
 
         HTTPClient http;
         
-        Serial.println("[DEBG] " + String(sen1) + String(sen2) + String(sen3) + String(temp));
-        http.begin("http://node03.popschool-willems.fr:1880/mod1/" + String(sen1)+ "/" + String(sen2) + "/" +String(sen3)+ "/" +String(temp));
+        Serial.println("[DEBG] " + String(sen) + String(temp));
+        http.begin("http://node03.popschool-willems.fr:1880/mod1/" + String(sen)+ "/" +String(temp));
         int httpCode = http.GET();
         if(httpCode > 0) {
             Serial.printf("[HTTP] GET... code: %d\n", httpCode);
