dd5ab92ca0315808ca567e243dfee4fb6054da91
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-12-06 23:45:52 +0100

    Update Module_cuve_confinement.ino

--- Module cuve/Module_cuve_confinement/Module_cuve_confinement.ino
+++ Module cuve/Module_cuve_confinement/Module_cuve_confinement.ino
@@ -32,20 +32,20 @@ void loop() {
   Serial.println(" %");
   Serial.println("-----------------");
 
-//if ((wifiMulti.run() == WL_CONNECTED)) {
-//    HTTPClient http;
-//    Serial.println("[DEBG] " + String(distance) + String(hauteur_restante) + String(pourcentage));
-//    http.begin("http://node03.popschool-willems.fr:1880/mod2/" + String(pourcentage));
-//    int httpCode = http.GET();
-//    if (httpCode > 0) {
-//      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-//      if (httpCode == HTTP_CODE_OK) {
-//        String payload = http.getString();
-//        Serial.println(payload);}}
-//    else {
-//      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-//    }
-//    http.end();}
+if ((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    Serial.println("[DEBG] " + String(distance) + String(hauteur_restante) + String(pourcentage));
+    http.begin("http://node03.popschool-willems.fr:1880/mod2/" + String(pourcentage));
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);}}
+    else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();}
   
   delay(5000);
 }
