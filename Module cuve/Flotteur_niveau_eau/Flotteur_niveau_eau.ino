b96931b23dbbf27bcb2f61275c8f9d29b816f6c5
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-12-10 14:38:50 +0100

    0

---
+++ Module cuve/Flotteur_niveau_eau/Flotteur_niveau_eau.ino
@@ -0,0 +1,29 @@
+int switchPin = 7; // Bouton connecté à la pin 2 (pull-up, LOW=bouton pressé)
+
+  int val;        // variable pour lire l'état de l'entrée
+  int val2;       // variable pour lire l'état de l'entrée (après un delai)
+  int buttonState;   // variable pour mémoriser l'état du bouton
+  int lightMode = 0;              // La lampe est elle allumée?
+
+void setup() {
+  pinMode(switchPin, INPUT);    // Le bouton est une entrée 
+  Serial.begin(115200);
+  
+  buttonState = digitalRead(switchPin);   // lecture de l'état initial
+}
+
+void loop(){
+  val = digitalRead(switchPin);      // Lecture de la valeur d'entrée
+  delay(10);                         // 10 millisecondes
+  val2 = digitalRead(switchPin);     // Relecture de l'entrée pour vérification de parasitage
+
+  if (val == val2) {                 // S'assurer que l'on a 2 lectures successives consistante!
+
+    if (val != buttonState) {          // Le bouton a changer d'état!
+      if (val == LOW) {                // Le bouton est il pressé?
+        Serial.println("On !");
+    }
+    buttonState = val;                 // Sauver le nouvel état du bouton dans une variable
+  }
+}
+}
