56e9f6e570b5041fbb6fa4dffab66508a6bcfbb3
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-12-10 15:08:20 +0100

    Update Flotteur_niveau_eau.ino

--- Module cuve/Flotteur_niveau_eau/Flotteur_niveau_eau.ino
+++ Module cuve/Flotteur_niveau_eau/Flotteur_niveau_eau.ino
@@ -1,32 +1,51 @@
-int switchPin = 2; // Bouton connecté à la pin 2 (pull-up, LOW=bouton pressé)
+const int flotteurbasPin = 7;     // the number of the pushbutton pin
+const int flotteurhautPin = 2;
+//const int relaisPin =  13;      // the number of the LED pin
 
-  int val;        // variable pour lire l'état de l'entrée
-  int val2;       // variable pour lire l'état de l'entrée (après un delai)
-  int buttonState;   // variable pour mémoriser l'état du bouton
+// variables will change:
+int flotteurbasState = 0;         // variable for reading the pushbutton status
+int flotteurhautState = 1;
 
 void setup() {
-  pinMode(switchPin, INPUT);    // Le bouton est une entrée 
+  // initialize the relais pin as an output:
+//  pinMode(relaisPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(flotteurbasPin, INPUT);
+  pinMode(flotteurhautPin, INPUT);
   Serial.begin(115200);
-  
-  buttonState = digitalRead(switchPin);   // lecture de l'état initial
 }
 
-void loop(){
-  val = digitalRead(switchPin);      // Lecture de la valeur d'entrée
-  Serial.print("Valeur 1 : ");
-  Serial.println(val);
-  delay(2000);                         // 10 millisecondes
-  val2 = digitalRead(switchPin);     // Relecture de l'entrée pour vérification de parasitage
-  Serial.print("Valeur 2 : ");
-  Serial.println(val2);
+void loop() {
+  // read the state of the flotteur value:
+  flotteurbasState = digitalRead(flotteurbasPin);
+  Serial.print("Lecture flotteur bas : ");
+  Serial.println(flotteurbasState);
+  flotteurhautState = digitalRead(flotteurhautPin);
+  Serial.print("Lecture flotteur haut : ");
+  Serial.println(flotteurhautState);
 
-  if (val == val2) {                 // S'assurer que l'on a 2 lectures successives consistante!
-
-    if (val != buttonState) {          // Le bouton a changer d'état!
-      if (val == LOW) {                // Le bouton est il pressé?
-        Serial.println("On !");
-    }
-    buttonState = val;                 // Sauver le nouvel état du bouton dans une variable
+  // check if the flotteurbas is up
+  // if it is, the flotteurbasState is HIGH:
+  if (flotteurbasState == LOW) {
+    // turn LED on:
+    //digitalWrite(relaisPin, HIGH); // pompe en marche
+    Serial.print("Fonctionnement flotteur bas : ");
+    Serial.println("On !");
+  } if (flotteurbasState == HIGH) {
+    // turn LED off:
+    //digitalWrite(relaisPin, LOW);
+    Serial.print("Fonctionnement flotteur bas : ");
+    Serial.println("Off !");
   }
-}
+  if (flotteurhautState == LOW) {
+   // turn led off
+  //  digitalWrite(relaisPin, LOW);
+  Serial.print("Fonctionnement flotteur haut : On !");
+  } if (flotteurhautState == HIGH) {
+   // turn LED on
+    //digitalWrite(relaisPin, HIGH);
+    Serial.print("Fonctionnement flotteur haut : ");
+    Serial.println("Off !");
+  }
+  delay(2000);
 }
