07459be176e446c75255c88ce725ad32f42572a6
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-12-14 15:20:44 +0100

    Update Flotteur_niveau_eau.ino

--- Module cuve/Flotteur_niveau_eau/Flotteur_niveau_eau.ino
+++ Module cuve/Flotteur_niveau_eau/Flotteur_niveau_eau.ino
@@ -1,26 +1,24 @@
-const unsigned long DUREE_MIN_OSMOL = 20000; // Durée minimum d'osmolation en milliseconde (20000ms = 20s)
- 
-// Déclarations des connexions sur carte et des variables
-const byte PIN_RELAIS = 12; // Numéro du pin pour le relais
-const byte PIN_CAPTEUR = 9; // Numéro du pin pour les capteurs de niveau
-unsigned long temps_debut_osmolation; // Variable de type long non signée pour enregristrer un temps
- 
-void setup() { // Initialisation du programme
-  pinMode(PIN_CAPTEUR, INPUT_PULLUP); // Affecte le mode Entrée au pin capteur. En l'absence de branchement, la résistance interne Pullup tire vers un état HIGH non aléatoire.
-  pinMode(PIN_RELAIS, OUTPUT); // Affecte le mode Sortie (envoi d'un signal) au pin du relais
-  Serial.begin(115200);
+const int Pushbutton = 4; //bouton-poussoir connecté à la broche D4
+const int LED = 7; //LED connectée à la broche D7
+int value = 0;     // Variable pour lire l'état du bouton-poussoir
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode (LED, OUTPUT); //La broche du LED est configurée comme sortie
+ pinMode (Pushbutton, INPUT); //La broche du bouton-poussoir est configurée comme entrée avec une resistance pull-up externe
 }
- 
-void loop() { // Programme en boucle
-  if (digitalRead(PIN_CAPTEUR) == LOW) { // Si le pin du capteur est LOW (contact pullup fermé, flotteur bas)
-    digitalWrite(PIN_RELAIS, HIGH); // Envoie du courant au pin relais, la pompe injecte de l'eau
-    Serial.println("Off");
-    temps_debut_osmolation = millis(); // Affecte le temps actuel au début d'osmolation
-  }
-  else { // Sinon, si le flotteur est en haut
-    if (millis() > temps_debut_osmolation + DUREE_MIN_OSMOL) { // Si la durée d'osmolation dépasse la durée min
-      digitalWrite(PIN_RELAIS, LOW); // N'envoie plus de courant sur le pin relais, la pompe s'arrête
-      Serial.println("On");
+
+void loop() {
+  // mettre votre code principal ici, qui va s'exécuter de façon répétée:
+value = digitalRead(Pushbutton); //Lire l'état du bouton-poussoir
+if (value == LOW) //Si le bouton est appuyé
+    {
+       delay(10); //10 milliseconds anti-rebond
+       if (value == LOW) //vérifier encore so le bouton est toujours appuyé
+    {
+        digitalWrite(LED, HIGH); //LED est allumée
+        delay(5000); //pause de 5 Second 
+        digitalWrite(LED, LOW); //LED est éteinte
     }
-  }
+    }
 }
