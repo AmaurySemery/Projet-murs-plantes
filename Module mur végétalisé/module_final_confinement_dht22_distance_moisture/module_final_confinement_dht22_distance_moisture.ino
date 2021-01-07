2d82b750a259fa31f6bc99e91bb3dd667bb6f1d6
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-01-07 22:27:14 +0100

    Update module_final_confinement_dht22_distance_moisture.ino

--- Module mur végétalisé/module_final_confinement_dht22_distance_moisture/module_final_confinement_dht22_distance_moisture.ino
+++ Module mur végétalisé/module_final_confinement_dht22_distance_moisture/module_final_confinement_dht22_distance_moisture.ino
@@ -1,4 +1,8 @@
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
 #include <UltrasonicSensor.h>
+#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
@@ -7,16 +11,21 @@
 #define DHTTYPE    DHT22
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
+WiFiMulti wifiMulti;
+
+const char* ssid = "freebox_IPLMWO";
+const char* password = "F4CAE54A9B34";
 
 UltrasonicSensor ultrasonic(2, 4);
 
 int hauteur_totale = 105; // modifier suivant la hauteur de la cuve
-int sensorPin = 8;
+int sensorPin = A1;
 float a = 40.95;
 int b = 100;
 
 void setup() {
   Serial.begin(115200);
+  wifiMulti.addAP(ssid, password);
   dht.begin();
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
@@ -46,7 +55,6 @@ void loop() {
   Serial.print("Niveau de remplissage de la cuve : ");
   Serial.print(pourcentage);
   Serial.println(" %");
-  Serial.println("-----------------");
 
   uint16_t value = analogRead(sensorPin);
   float division = value / a;
@@ -55,6 +63,30 @@ void loop() {
   Serial.print(sensorconvert);
   Serial.println(" %");
   Serial.println("----------------");
-  
+
+  int sen = sensorconvert;
+  int temp = event.temperature;
+
+  if((wifiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+        
+        Serial.println("[DEBG] " + String(sen) + String(temp) + String(distance) + String(hauteur_restante) + String(pourcentage));
+        http.begin("http://node03.popschool-willems.fr:1880/mod1/" + String(sen)+ "/" +String(temp) + "/" + String(pourcentage));
+        int httpCode = http.GET();
+        if(httpCode > 0) {
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                Serial.println("Réception du message à partir de Node-Red : ok");
+                Serial.println(payload);
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            Serial.println("Réception du message à partir de Node-Red : échec");
+        }
+
+        http.end();
+    }
   delay(5000);
 }
