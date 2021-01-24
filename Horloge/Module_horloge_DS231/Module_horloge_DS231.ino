67dceae53d9cb770c71647cac054237a953d9043
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-01-24 16:28:14 +0100

    Update Module_horloge_DS231.ino

--- Horloge/Module_horloge_DS231/Module_horloge_DS231.ino
+++ Horloge/Module_horloge_DS231/Module_horloge_DS231.ino
@@ -1,146 +1,104 @@
-#include <Wire.h>
-#include "ds3231.h"
-
-#define BUFF_MAX 128
-
-uint8_t time[8];
-char recv[BUFF_MAX];
-unsigned int recv_size = 0;
-unsigned long prev, interval = 5000;
-
-void parse_cmd(char *cmd, int cmdsize);
-
-void setup()
-{
-    Serial.begin(9600);
-    Wire.begin();
-    DS3231_init(DS3231_CONTROL_INTCN);
-    memset(recv, 0, BUFF_MAX);
-    Serial.println("GET time");
+//Libraries
+#include <Wire.h>//https://www.arduino.cc/en/reference/wire
+#include <DS3231.h>//https://github.com/NorthernWidget/DS3231
+//Variables
+byte Year ;
+byte Month ;
+byte Date ;
+byte DoW ;
+byte Hour ;
+byte Minute ;
+byte Second ;
+bool Century  = false;
+bool h12 ;
+bool PM ;
+//Objects
+DS3231 Clock;
+void setup() {
+ //Init Serial USB
+ Serial.begin(9600);
+ Serial.println(F("Initialize System"));
+ Wire.begin();
 }
-
-void loop()
-{
-    char in;
-    char buff[BUFF_MAX];
-    unsigned long now = millis();
-    struct ts t;
-
-    // show time once in a while
-    if ((now - prev > interval) && (Serial.available() <= 0)) {
-        DS3231_get(&t);
-
-        // there is a compile time option in the library to include unixtime support
-#ifdef CONFIG_UNIXTIME
-#ifdef __AVR__
-        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d %ld", t.year,
-#error AVR
-#else
-        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d %d", t.year,
-#endif
-             t.mon, t.mday, t.hour, t.min, t.sec, t.unixtime);
-#else
-        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", t.year,
-             t.mon, t.mday, t.hour, t.min, t.sec);
-#endif
-
-        Serial.println(buff);
-        prev = now;
-    }
-
-    if (Serial.available() > 0) {
-        in = Serial.read();
-
-        if ((in == 10 || in == 13) && (recv_size > 0)) {
-            parse_cmd(recv, recv_size);
-            recv_size = 0;
-            recv[0] = 0;
-        } else if (in < 48 || in > 122) {;       // ignore ~[0-9A-Za-z]
-        } else if (recv_size > BUFF_MAX - 2) {   // drop lines that are too long
-            // drop
-            recv_size = 0;
-            recv[0] = 0;
-        } else if (recv_size < BUFF_MAX - 2) {
-            recv[recv_size] = in;
-            recv[recv_size + 1] = 0;
-            recv_size += 1;
-        }
-
-    }
+void loop() {
+ setDate();
+ readRTC();
 }
-
-void parse_cmd(char *cmd, int cmdsize)
-{
-    uint8_t i;
-    uint8_t reg_val;
-    char buff[BUFF_MAX];
-    struct ts t;
-
-    //snprintf(buff, BUFF_MAX, "cmd was '%s' %d\n", cmd, cmdsize);
-    //Serial.print(buff);
-
-    // TssmmhhWDDMMYYYY aka set time
-    if (cmd[0] == 84 && cmdsize == 16) {
-        //T355720619112011
-        t.sec = inp2toi(cmd, 1);
-        t.min = inp2toi(cmd, 3);
-        t.hour = inp2toi(cmd, 5);
-        t.wday = cmd[7] - 48;
-        t.mday = inp2toi(cmd, 8);
-        t.mon = inp2toi(cmd, 10);
-        t.year = inp2toi(cmd, 12) * 100 + inp2toi(cmd, 14);
-        DS3231_set(t);
-        Serial.println("OK");
-    } else if (cmd[0] == 49 && cmdsize == 1) {  // "1" get alarm 1
-        DS3231_get_a1(&buff[0], 59);
-        Serial.println(buff);
-    } else if (cmd[0] == 50 && cmdsize == 1) {  // "2" get alarm 1
-        DS3231_get_a2(&buff[0], 59);
-        Serial.println(buff);
-    } else if (cmd[0] == 51 && cmdsize == 1) {  // "3" get aging register
-        Serial.print("aging reg is ");
-        Serial.println(DS3231_get_aging(), DEC);
-    } else if (cmd[0] == 65 && cmdsize == 9) {  // "A" set alarm 1
-        DS3231_set_creg(DS3231_CONTROL_INTCN | DS3231_CONTROL_A1IE);
-        //ASSMMHHDD
-        for (i = 0; i < 4; i++) {
-            time[i] = (cmd[2 * i + 1] - 48) * 10 + cmd[2 * i + 2] - 48; // ss, mm, hh, dd
-        }
-        uint8_t flags[5] = { 0, 0, 0, 0, 0 };
-        DS3231_set_a1(time[0], time[1], time[2], time[3], flags);
-        DS3231_get_a1(&buff[0], 59);
-        Serial.println(buff);
-    } else if (cmd[0] == 66 && cmdsize == 7) {  // "B" Set Alarm 2
-        DS3231_set_creg(DS3231_CONTROL_INTCN | DS3231_CONTROL_A2IE);
-        //BMMHHDD
-        for (i = 0; i < 4; i++) {
-            time[i] = (cmd[2 * i + 1] - 48) * 10 + cmd[2 * i + 2] - 48; // mm, hh, dd
-        }
-        uint8_t flags[5] = { 0, 0, 0, 0 };
-        DS3231_set_a2(time[0], time[1], time[2], flags);
-        DS3231_get_a2(&buff[0], 59);
-        Serial.println(buff);
-    } else if (cmd[0] == 67 && cmdsize == 1) {  // "C" - get temperature register
-        Serial.print("temperature reg is ");
-        Serial.println(DS3231_get_treg(), DEC);
-    } else if (cmd[0] == 68 && cmdsize == 1) {  // "D" - reset status register alarm flags
-        reg_val = DS3231_get_sreg();
-        reg_val &= B11111100;
-        DS3231_set_sreg(reg_val);
-    } else if (cmd[0] == 70 && cmdsize == 1) {  // "F" - custom fct
-        reg_val = DS3231_get_addr(0x5);
-        Serial.print("orig ");
-        Serial.print(reg_val,DEC);
-        Serial.print("month is ");
-        Serial.println(bcdtodec(reg_val & 0x1F),DEC);
-    } else if (cmd[0] == 71 && cmdsize == 1) {  // "G" - set aging status register
-        DS3231_set_aging(0);
-    } else if (cmd[0] == 83 && cmdsize == 1) {  // "S" - get status register
-        Serial.print("status reg is ");
-        Serial.println(DS3231_get_sreg(), DEC);
-    } else {
-        Serial.print("unknown command prefix ");
-        Serial.println(cmd[0]);
-        Serial.println(cmd[0], DEC);
-    }
+void readRTC( ) { /* function readRTC */
+ ////Read Real Time Clock
+ Serial.print(Clock.getYear(), DEC);
+ Serial.print("-");
+ Serial.print(Clock.getMonth(Century), DEC);
+ Serial.print("-");
+ Serial.print(Clock.getDate(), DEC);
+ Serial.print(" ");
+ Serial.print(Clock.getHour(h12, PM), DEC); //24-hr
+ Serial.print(":");
+ Serial.print(Clock.getMinute(), DEC);
+ Serial.print(":");
+ Serial.println(Clock.getSecond(), DEC);
+ delay(1000);
+}
+void setDate( ) { /* function setDate */
+ ////Set Real Time Clock
+ if (Serial.available()) {
+   //int _start = millis();
+   GetDateStuff(Year, Month, Date, DoW, Hour, Minute, Second);
+   Clock.setClockMode(false);  // set to 24h
+   Clock.setSecond(Second);
+   Clock.setMinute(Minute);
+   Clock.setHour(Hour);
+   Clock.setDate(Date);
+   Clock.setMonth(Month);
+   Clock.setYear(Year);
+   Clock.setDoW(DoW);
+ }
+}
+void GetDateStuff(byte& Year, byte& Month, byte& Day, byte& DoW, byte& Hour, byte& Minute, byte& Second) { /* function GetDateStuff */
+ ////Get date data
+ // Call this if you notice something coming in on
+ // the serial port. The stuff coming in should be in
+ // the order YYMMDDwHHMMSS, with an 'x' at the end.
+ boolean GotString = false;
+ char InChar;
+ byte Temp1, Temp2;
+ char InString[20];
+ byte j = 0;
+ while (!GotString) {
+   if (Serial.available()) {
+     InChar = Serial.read();
+     InString[j] = InChar;
+     j += 1;
+     if (InChar == 'x') {
+       GotString = true;
+     }
+   }
+ }
+ Serial.println(InString);
+ // Read Year first
+ Temp1 = (byte)InString[0] - 48;
+ Temp2 = (byte)InString[1] - 48;
+ Year = Temp1 * 10 + Temp2;
+ // now month
+ Temp1 = (byte)InString[2] - 48;
+ Temp2 = (byte)InString[3] - 48;
+ Month = Temp1 * 10 + Temp2;
+ // now date
+ Temp1 = (byte)InString[4] - 48;
+ Temp2 = (byte)InString[5] - 48;
+ Day = Temp1 * 10 + Temp2;
+ // now Day of Week
+ DoW = (byte)InString[6] - 48;
+ // now Hour
+ Temp1 = (byte)InString[7] - 48;
+ Temp2 = (byte)InString[8] - 48;
+ Hour = Temp1 * 10 + Temp2;
+ // now Minute
+ Temp1 = (byte)InString[9] - 48;
+ Temp2 = (byte)InString[10] - 48;
+ Minute = Temp1 * 10 + Temp2;
+ // now Second
+ Temp1 = (byte)InString[11] - 48;
+ Temp2 = (byte)InString[12] - 48;
+ Second = Temp1 * 10 + Temp2;
 }
