1d672f5ab718dc34195313003bb4b12fbfd5d852
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-01-24 17:19:06 +0100

    Update Module_horloge_DS231.ino

--- Horloge/Module_horloge_DS231/Module_horloge_DS231.ino
+++ Horloge/Module_horloge_DS231/Module_horloge_DS231.ino
@@ -23,6 +23,7 @@ void setup() {
  Wire.begin();
 }
 void loop() {
+ setDate();
  readRTC();
  if(Clock.getMinute() == 2 and Clock.getSecond() == 0){Serial.println("Hello !");
  digitalWrite(ledpin, HIGH);
@@ -43,4 +44,67 @@ void readRTC( ) { /* function readRTC */
  Serial.print(":");
  Serial.println(Clock.getSecond(), DEC);
  delay(1000);
+}
+void setDate( ) { /* function setDate */
+ ////Set Real Time Clock
+ if (Serial.available()) {
+   //int _start = millis();
+   GetDateStuff(Year, Month, Date, DoW, Hour, Minute, Second);
+   Clock.setClockMode(false);  // set to 24h
+   Clock.setSecond(Second);
+   Clock.setMinute(Minute);
+   Clock.setHour(Hour);
+   Clock.setDate(Date);
+   Clock.setMonth(Month);
+   Clock.setYear(Year);
+   Clock.setDoW(DoW);
+ }
+}
+void GetDateStuff(byte& Year, byte& Month, byte& Day, byte& DoW, byte& Hour, byte& Minute, byte& Second) { /* function GetDateStuff */
+ ////Get date data
+ // Call this if you notice something coming in on
+ // the serial port. The stuff coming in should be in
+ // the order YYMMDDwHHMMSS, with an 'x' at the end.
+ boolean GotString = false;
+ char InChar;
+ byte Temp1, Temp2;
+ char InString[20];
+ byte j = 0;
+ while (!GotString) {
+   if (Serial.available()) {
+     InChar = Serial.read();
+     InString[j] = InChar;
+     j += 1;
+     if (InChar == 'x') {
+       GotString = true;
+     }
+   }
+ }
+ Serial.println(InString);
+ // Read Year first
+ Temp1 = (byte)InString[0] - 48;
+ Temp2 = (byte)InString[1] - 48;
+ Year = Temp1 * 10 + Temp2;
+ // now month
+ Temp1 = (byte)InString[2] - 48;
+ Temp2 = (byte)InString[3] - 48;
+ Month = Temp1 * 10 + Temp2;
+ // now date
+ Temp1 = (byte)InString[4] - 48;
+ Temp2 = (byte)InString[5] - 48;
+ Day = Temp1 * 10 + Temp2;
+ // now Day of Week
+ DoW = (byte)InString[6] - 48;
+ // now Hour
+ Temp1 = (byte)InString[7] - 48;
+ Temp2 = (byte)InString[8] - 48;
+ Hour = Temp1 * 10 + Temp2;
+ // now Minute
+ Temp1 = (byte)InString[9] - 48;
+ Temp2 = (byte)InString[10] - 48;
+ Minute = Temp1 * 10 + Temp2;
+ // now Second
+ Temp1 = (byte)InString[11] - 48;
+ Temp2 = (byte)InString[12] - 48;
+ Second = Temp1 * 10 + Temp2;
 }
