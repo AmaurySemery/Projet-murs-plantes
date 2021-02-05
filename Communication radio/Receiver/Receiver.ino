673c87777d83d5d1726b9693d5f767d9a17c6ddb
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-02-05 13:22:47 +0100

    Update Receiver.ino

--- Communication radio/Receiver/Receiver.ino
+++ Communication radio/Receiver/Receiver.ino
@@ -34,6 +34,7 @@ RadioPacket _radioData;
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("Hello !");
 
     // By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
     // Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
@@ -43,7 +44,7 @@ void setup()
     //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
     //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
     
-    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100))
     {
         Serial.println("Cannot communicate with radio");
         while (1); // Wait here forever.
