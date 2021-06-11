8c79b85e2ac4dafc3ebd50425a59953420e44fe9
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-06-11 18:25:49 +0200

    Update Test2.py

--- Dataframes python/Test2.py
+++ Dataframes python/Test2.py
@@ -2,9 +2,10 @@ import pandas as pd
 from time import sleep
 import csv
 from datetime import datetime
+import time
 
 i = 0
-
+range = 10
 Entree420mA = '4-20mA'
 EntréeTension = '0-20V'
 EntréeAutre = 'Autre'
@@ -20,7 +21,7 @@ now = datetime.now()
 
 # Dataframes du Sensor 1 dans le Can Bus 1 pour la Sensor Box 1
 dfC1SB1S1 = pd.DataFrame(columns=['Type','Value','Input Type','Product Ref','Brand','Name','Date'])
-while i < 1:
+while i < range:
     i = i + 1
     Type = 'Moisture Sensor'
     Value = 2 # Placer la valeur du return de la fonction get du sensor ici
@@ -32,7 +33,17 @@ while i < 1:
     Date = time.strftime(FormatDate)
     sleep(sleepsecond)
     dfC1SB1S1 = dfC1SB1S1.append({'Type': Type,'Value': Value,'Input Type': InputType,'Product Ref': ProductRef,'Brand': Brand,'Name': Name,'Date': Date}, ignore_index=True)
-    #print(dfC1SB1S1)
+    print(dfC1SB1S1)
+    if(i >= range):
+        FormatDateLog = '%d-%m-%y' + '-' + '%H-%M-%S'
+        lognow = time.strftime(FormatDateLog)
+        LogdfC1SB1S1 = '/Users/Fuan/Desktop/Solioti/test/soliotitest'+lognow+'.csv'
+        print(LogdfC1SB1S1)
+        df = pd.DataFrame(dfC1SB1S1) # placer la liste des dataframes ici
+        df.to_csv(LogdfC1SB1S1) # placer l'adresse du fichier ici
+        i = 0
+        del dfC1SB1S1
+        dfC1SB1S1 = pd.DataFrame(columns=['Type','Value','Input Type','Product Ref','Brand','Name','Date'])
 
 
 print(dfC1SB1S1)
@@ -41,10 +52,6 @@ print(dfC1SB1S1)
 
 
 # Ajouter fonction if quand on atteint une range de 10 pour générer le fichier
-lognow = '-' + str(now.day) + '-' + str(now.month) + '-' + str(now.year) + '-' + str(now.hour) + 'h-' + str(now.minute) + 'm-' + str(now.second) + 's'
-LogdfC1SB1S1 = '/Users/Fuan/Desktop/Solioti/test/soliotitest'+lognow+'.csv'
-print(LogdfC1SB1S1)
-df = pd.DataFrame(dfC1SB1S1) # placer la liste des dataframes ici
-df.to_csv(LogdfC1SB1S1) # placer l'adresse du fichier ici
+
 
 # Un nouveau fichier d'historiques est généré dans le dossier visé
