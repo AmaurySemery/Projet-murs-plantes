910122d6255bbbb2ea20807128fb6699c679200f
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-06-11 14:53:15 +0200

    Update Test.py

--- Dataframes python/Test.py
+++ Dataframes python/Test.py
@@ -1,9 +1,14 @@
 import pandas as pd
-import numpy as np
-import json
 from time import sleep
+import csv
+from datetime import datetime
 
-a=1
+ra= 5
+Entree420mA = '4-20mA'
+EntréeTension = '0-20V'
+EntréeAutre = 'Autre'
+BrandAdafruit = 'Adafruit'
+ProductRefVMSB = 'VM-SB'
 sleepmillisecond=0.1
 sleepsecond=1
 sleep10second=10
@@ -11,12 +16,18 @@ sleep30second=30
 sleepminut=60
 sleephour=3600
 
-
-df = pd.DataFrame(columns=['EV1','EV2'])
-for i in range(a+1):
-    i = 1
-    b = i + 1
-    c = i - 1
-    df = df.append({'EV1': b,'EV2' : c}, ignore_index=True)
-    print(df)
+# Dataframes du Sensor 1 dans le Can Bus 1 pour la Sensor Box 1
+dfC1SB1S1 = pd.DataFrame(columns=['Type','Value','Input Type','Product Ref','Brand','Name','Date'])
+for i in range(ra):
+    Type = 'Moisture Sensor'
+    Value = 2 # Placer le return de la fonction get du sensor ici
+    InputType = Entree420mA
+    ProductRef = ProductRefVMSB
+    Brand = BrandAdafruit
+    Name = 'Régis'
+    Date = datetime.now()
+    dfC1SB1S1 = dfC1SB1S1.append({'Type': Type,'Value': Value,'Input Type': InputType,'Product Ref': ProductRef,'Brand': Brand,'Name': Name,'Date': Date}, ignore_index=True)
+    #print(dfC1SB1S1)
     sleep(sleepsecond)
+
+print(dfC1SB1S1)
