57a693d8c8f8410d506340094e3e08876190114d
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-06-11 12:40:36 +0200

    Create Dataframes.py

---
+++ Dataframes python/Dataframes.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import json
+
+#f=open(r"C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\classement_SSAoG_CSV.csv")
+
+dEV = {'EV1': [], 'EV2': [], 'EV3': [1, 2], 'EV4': [3, 4],'EV5': [1, 2], 'EV6': [3, 4],'EV7': [1, 2], 'EV8': [3, 4],'EV9': [1, 2]}
+dfEV = pd.DataFrame(data=d)
+dfEV
