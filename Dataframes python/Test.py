a073000bb40dc567c294ce95320f0c84c360f713
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-06-11 13:57:07 +0200

    Update Test.py

--- Dataframes python/Test.py
+++ Dataframes python/Test.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
 import json
+from time import sleep
 
 a=1
+sleepsecond=1
+sleep10second=10
+sleep30second=30
+sleepminut=60
+sleephour=3600
 
-df = pd.DataFrame({'age':    [a],
-                   'height': [a],
-                   'weight': [a]})
+
+df = pd.DataFrame(columns=['EV1','EV2'])
+for i in range(5):
+    i = 1
+    b = i + 1
+    c = i - 1
+    df = df.append({'EV1': b,'EV2' : c}, ignore_index=True)
+    print(df)
+    sleep(sleepsecond)
