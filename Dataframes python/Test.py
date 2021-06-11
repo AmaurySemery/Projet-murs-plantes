2e1455abba3cbe797d49d45390f1cc4e01d1206c
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-06-11 14:02:25 +0200

    Update Test.py

--- Dataframes python/Test.py
+++ Dataframes python/Test.py
@@ -4,6 +4,7 @@ import json
 from time import sleep
 
 a=1
+sleepmillisecond=0.1
 sleepsecond=1
 sleep10second=10
 sleep30second=30
@@ -12,7 +13,7 @@ sleephour=3600
 
 
 df = pd.DataFrame(columns=['EV1','EV2'])
-for i in range(5):
+for i in range(a+1):
     i = 1
     b = i + 1
     c = i - 1
