64706ba0736bfdcbbb3f3e362e88cb5703b6169e
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2021-06-11 18:03:04 +0200

    Update Test.py

--- Dataframes python/Test.py
+++ Dataframes python/Test.py
@@ -20,16 +20,18 @@ now = datetime.now()
 # Dataframes du Sensor 1 dans le Can Bus 1 pour la Sensor Box 1
 dfC1SB1S1 = pd.DataFrame(columns=['Type','Value','Input Type','Product Ref','Brand','Name','Date'])
 for i in range(ra):
+    now
     Type = 'Moisture Sensor'
     Value = 2 # Placer la valeur du return de la fonction get du sensor ici
     InputType = Entree420mA
     ProductRef = ProductRefVMSB
     Brand = BrandAdafruit
     Name = 'Regis'
-    Date = str(now.day) + '/' + str(now.month) + '/'+ str(now.year) + '-' + str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
-    sleep(sleepsecond)
+    FormatDate = '%d/%m/%y' + '-' + '%H:%M:%S'
+    Date = time.strftime(FormatDate)
     dfC1SB1S1 = dfC1SB1S1.append({'Type': Type,'Value': Value,'Input Type': InputType,'Product Ref': ProductRef,'Brand': Brand,'Name': Name,'Date': Date}, ignore_index=True)
-    #print(dfC1SB1S1)
+    print(dfC1SB1S1)
+    sleep(sleepsecond)
 
 
 print(dfC1SB1S1)
